9ae9634e103510adb6b85b67f53bccb3b3862180
naszifnaaz <naszifnaaz@gmail.com>
2022-09-01 14:49:51 +0530

    ci: solving-sudoku-using-backtracking

--- sudoku.py
+++ sudoku.py
@@ -26,4 +26,58 @@ def print_board(board):
                 print(str(board[i][j])+" ", end="")
 
 
+# Finding empty grid and returning its index
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)
+
+
+# Checking for Valid solution
+def valid(bo, num, pos):
+    # Check Rows
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check Column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Checking 3x3 grid
+    box_x = pos[0] // 3
+    box_y = pos[1] // 3
+
+    for i in range(box_x * 3, box_x * 3 + 3):
+        for j in range(box_y * 3, box_y * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+
+# Backtracking algorithm to solve puzzle
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+            bo[row][col] = 0
+    return False
+
+
+print_board(board)
+solve(board)
+print("\n")
+print("Solved! ")
+print("\n")
 print_board(board)
